196ddd6f94813b93945c542e073e23d74d5bf76e
abdul wahab <abdul.wahab@codeforesight.com>
2023-07-16 17:25:38 +0500

    Added: Create checkout component for cart and update shoppingcart component functionality

---
+++ aliexpress/pages/checkout.vue
@@ -0,0 +1,122 @@
+<template>
+    <MainLayout>
+        <div id="CheckoutPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+            <div class="md:flex gap-4 justify-between mx-auto w-full">
+                <div class="md:w-[65%]">
+                    <div class="bg-white rounded-lg p-4">
+                        <div class="text-xl font-semibold mb-2">Shipping Address</div>
+                        <div v-if="true">
+                            <NuxtLink
+                                to="/address"
+                                class="flex items-center pb-2 text-blue-500 hover:text-red-400"
+                            >
+                                <Icon name="mdi:plus" size="18" class="mr-2"/>
+                                Update Address
+                            </NuxtLink>
+
+                            <div class="pt-2 border-t">
+                                <div class="underline pb-1">Delivery Address</div>
+                                <ul class="text-xs">
+                                    <li class="flex items-center gap-2">
+                                        <div>Contact name:</div>
+                                        <div class="font-semibold">Test</div>
+                                    </li>
+                                    <li class="flex items-center gap-2">
+                                        <div>Address:</div>
+                                        <div class="font-semibold">Test</div>
+                                    </li>
+                                    <li class="flex items-center gap-2">
+                                        <div>Zip Code:</div>
+                                        <div class="font-semibold">Test</div>
+                                    </li>
+                                    <li class="flex items-center gap-2">
+                                        <div>City:</div>
+                                        <div class="font-semibold">Test</div>
+                                    </li>
+                                    <li class="flex items-center gap-2">
+                                        <div>Country:</div>
+                                        <div class="font-semibold">Test</div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <NuxtLink
+                            v-else
+                            to="/address"
+                            class="flex items-center text-blue-500 hover:text-red-400"
+                        >
+                            <Icon name="mdi:plus" size="18" class="mr-2"/>
+                            Add New Address
+                        </NuxtLink>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from '../layouts/MainLayout.vue';
+import { useUserStore } from '../stores/user';
+import { useRoute } from '../.nuxt/vue-router';
+
+const userStore = useUserStore();
+const route = useRoute();
+
+let stripe = null;
+let element = null;
+let card = null;
+let form = null;
+let total = ref(0);
+let clientSecret = null;
+let currentAddress = ref(null);
+let isProcessing = ref(false);
+
+const products = [
+    {
+        id: 1,
+        title: "Title 1",
+        description: "This is a description",
+        url: "https://picsum.photos/id/7/800/800",
+        price: 9988,
+    },
+    {
+        id: 2,
+        title: "Title 2",
+        description: "This is a description",
+        url: "https://picsum.photos/id/71/800/800",
+        price: 9999,
+    },
+];
+
+onMounted(() => {
+    isProcessing.value = true;
+
+    userStore.checkout.forEach((item) => {
+        total.value = total.value + item.price;
+    })
+})
+
+watch(() => total.value, () => {
+    if(total.value > 0) {
+        stripeInit();
+    }
+})
+
+const stripeInit = async() => {
+
+}
+
+const pay = async() => {
+
+}
+
+const createOrder = async(stripeId) => {
+
+}
+
+const showError = (errorMsgText) => {
+
+}
+</script>
