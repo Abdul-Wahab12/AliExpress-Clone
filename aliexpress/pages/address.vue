55f2c6ce65bb6dcf949134e801fffd426e711735
abdul wahab <abdul.wahab@codeforesight.com>
2023-07-17 22:54:27 +0500

    Added: add checkout, textinput and address component for cart purposes

---
+++ aliexpress/pages/address.vue
@@ -0,0 +1,37 @@
+<template>
+    <MainLayout>
+        <div id="AddressPage" class="bg-white mt-4 max-w-[500px] mx-auto px-2">
+            <div class="mx-auto bg-white rounded-lg p-3">
+                <div class="text-xl font-bold mb-2">Address Details</div>
+
+                <form @submit.prevent="submit()">
+                    <TextInput 
+                        class="w-full"
+                        placeholder="Contact Name"
+                        v-model:input="contactName"
+                        inputType="text"
+                        :error="error && error.type == 'contactName' ? error.message : ''"
+                    />
+                    <TextInput 
+                        class="w-full mt-2"
+                        placeholder="Address"
+                        v-model:input="address"
+                        inputType="text"
+                        :error="error && error.type == 'address' ? error.message : ''"
+                    />
+                    <TextInput 
+                        class="w-full mt-2"
+                        placeholder="Zip Code"
+                        v-model:input="zipCode"
+                        inputType="text"
+                        :error="error && error.type == 'zipCode' ? error.message : ''"
+                    />
+                </form>
+            </div>
+        </div>
+    </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from '../layouts/MainLayout.vue';
+</script>
--- aliexpress/pages/checkout.vue
+++ aliexpress/pages/checkout.vue
@@ -5,12 +5,9 @@
                 <div class="md:w-[65%]">
                     <div class="bg-white rounded-lg p-4">
                         <div class="text-xl font-semibold mb-2">Shipping Address</div>
-                        <div v-if="true">
-                            <NuxtLink
-                                to="/address"
-                                class="flex items-center pb-2 text-blue-500 hover:text-red-400"
-                            >
-                                <Icon name="mdi:plus" size="18" class="mr-2"/>
+                        <div v-if="false">
+                            <NuxtLink to="/address" class="flex items-center pb-2 text-blue-500 hover:text-red-400">
+                                <Icon name="mdi:plus" size="18" class="mr-2" />
                                 Update Address
                             </NuxtLink>
 
@@ -41,14 +38,56 @@
                             </div>
                         </div>
 
-                        <NuxtLink
-                            v-else
-                            to="/address"
-                            class="flex items-center text-blue-500 hover:text-red-400"
-                        >
-                            <Icon name="mdi:plus" size="18" class="mr-2"/>
+                        <NuxtLink v-else to="/address" class="flex items-center text-blue-500 hover:text-red-400">
+                            <Icon name="mdi:plus" size="18" class="mr-2" />
                             Add New Address
                         </NuxtLink>
+                    </div>
+
+                    <div id="Items" class="bg-white rounded-lg p-4 mt-4">
+                        <div v-for="product in products" :key="product">
+                            <CheckoutItem :product="product" />
+                        </div>
+                    </div>
+                </div>
+
+                <div class="md:hidden block my-4" />
+                <div class="md:w-[35%]">
+                    <div id="PlaceOrder" class="bg-white rounded-lg p-4">
+                        <div class="text-2xl font-extrabold mb-2">Summary</div>
+
+                        <div class="flex items-center justify-between my-4">
+                            <div>Total Shipping</div>
+                            <div>Free</div>
+                        </div>
+
+                        <div class="border-t" />
+
+                        <div class="flex items-center justify-between my-4">
+                            <div class="font-semibold">Total</div>
+                            <div class="text-2xl font-semibold">
+                                $ <span class="font-extrabold">{{ total / 100 }}</span>
+                            </div>
+                        </div>
+
+                        <form @submit.prevent="pay()">
+                            <div id="card-element" class="border border-gray-500 p-2 rounded-sm" />
+                            <p id="card-error" role="alert" class="text-red-700 text-center font-semibold"/>
+                            <button
+                                :disabled="isProcessing"
+                                type="submit"
+                                class="mt-4 bg-gradient-to-r from-[#FE630C] to-[#FF3200] w-full text-white text-[21px] font-semibold p-1.5 rounded-full"
+                                :class="isProcessing ? 'opacity-70' : 'opacity-100'"
+                                >
+                                    <Icon v-if="isProcessing" name="eos-icons:loading"/>
+                                    <div v-else>Place order</div>
+                            </button>
+                        </form>
+                    </div>
+
+                    <div class="bg-white rounded-lg p-4 mt-4">
+                        <div class="text-lg font-semibold mb-2 mt-2">AliExpress</div>
+                        <p class="my-2">AliExpress keeps your information and payment safe</p>
                     </div>
                 </div>
             </div>
@@ -99,20 +138,20 @@ onMounted(() => {
 })
 
 watch(() => total.value, () => {
-    if(total.value > 0) {
+    if (total.value > 0) {
         stripeInit();
     }
 })
 
-const stripeInit = async() => {
+const stripeInit = async () => {
 
 }
 
-const pay = async() => {
+const pay = async () => {
 
 }
 
-const createOrder = async(stripeId) => {
+const createOrder = async (stripeId) => {
 
 }
 
